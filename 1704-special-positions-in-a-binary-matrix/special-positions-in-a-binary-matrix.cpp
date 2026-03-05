017978aa64b885f0c8dcdec04c95aa05f6eb51b1
Saketku18 <saket123yadav@gmail.com>
2026-03-05 11:15:08 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (73.84%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
@@ -4,26 +4,32 @@ public:
         int m=mat.size();
         int n=mat[0].size();
         int count=0;
+        vector<bool> v(m,0);
+        vector<bool> vi(n,0);
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(mat[i][j]==1){
+                if(mat[i][j]==1 && !v[i] && !vi[j]){
                     bool f=1;
-                    for(int k=0;k<m;k++){
-                        if(k!=i && mat[k][j]==1){
-                        f=0;
-                        break;
-                        }
+                v[i]=1;
+                vi[j]=1;
+                for(int k=0;k<m;k++){
+                    if(k!=i && mat[k][j]==1){
+                    f=0;
+                    v[k]=1;
+                    break;
                     }
-                    for(int p=0;p<n;p++){
-                        if( p!=j && mat[i][p]==1){
-                        f=0;
-                        break;
-                        }
-                    }
-                    if(f)
-                    count++;
                 }
+                for(int p=0;p<n;p++){
+                    if(p!=j && mat[i][p]==1){
+                        f=0;
+                        vi[p]=1;
+                        break;
+                    }
+                }
+                if(f)
+                count++;
             }
+        }
         }
         return count;
     }
