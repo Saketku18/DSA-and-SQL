7ba5dcc74186ade1f839c594fac1abcd27ccdfa4
Saketku18 <saket123yadav@gmail.com>
2026-03-25 17:38:45 +0530

    Time: 225 ms (75.78%) | Memory: 111.7 MB (79.20%) - LeetSync

--- 3905-partition-string/partition-string.cpp
+++ 3905-partition-string/partition-string.cpp
@@ -3,7 +3,7 @@ public:
     vector<string> partitionString(string s) {
         vector<string> ans;
         string str="";
-        set<string> st;
+        unordered_set<string> st;
         for(int i=0;i<s.size();i++){
             str+=s[i];
             if(st.find(str)==st.end()){
