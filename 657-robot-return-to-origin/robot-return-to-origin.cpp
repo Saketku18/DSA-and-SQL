8d680507de60116d1ed798a7278327e8ae7d4e53
Saketku18 <saket123yadav@gmail.com>
2026-04-06 00:04:14 +0530

    Time: 4 ms (27.83%) | Memory: 10.4 MB (47.26%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int i=0;
+        int j=0;
+        for(int k=0;k<moves.size();k++){
+            if(moves[k]=='R')
+            j++;
+            else if(moves[k]=='L')
+            j--;
+            else if(moves[k]=='U')
+            i--;
+            else
+            i++;
+        }
+        if(i==0 && j==0)
+        return true;
+        return false;   
+    }
+};
